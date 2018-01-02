b8ca380fb9551e97a77fdb997833afa30a0f7d29
farz.bhullar@gmail.com <farz.bhullar@gmail.com>
2018-01-02 19:13:23 +0530

    url changes.

--- secure_app/urls.py
+++ secure_app/urls.py
@@ -21,9 +21,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', auth_views.login, {'template_name': 'login.html'}, name='root'),
+    url(r'^$', auth_views.login, {'template_name': 'login.html','redirect_authenticated_user': True}, name='root'),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html', 'redirect_authenticated_user': True}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='/'),
     url(r'^dashboard/', include('dashboard.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
